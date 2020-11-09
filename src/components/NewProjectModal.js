60e1efe99e369f37645eb35c7fd47a1fb26952db
Belen Corvalan <bcorvalan@4implus.com>
2020-11-09 13:11:11 -0300

    formValidation + formStyle

--- src/components/NewProjectModal.js
+++ src/components/NewProjectModal.js
@@ -1,75 +1,124 @@
-import React from 'react'
-import { useFormik } from 'formik'
+import React from "react";
+import { useFormik } from "formik";
 
 const initialValues = {
-    brand: '',
-    projectName: ''
-}
+  brand: "",
+  projectName: "",
+};
 
-const onSubmit = values => {
-    console.log('Form data', values);
-}
+const onSubmit = (values) => {
+  console.log("Form data", values);
+};
 
-const validate = values => {
-    let errors = {};
-    if(!values.brand){
-        errors.brand = 'Required';
-    }
-    if(!values.projectName){
-        errors.projectName = 'Required';
-    }
-    return errors
-}
-
+const validate = (values) => {
+  let errors = {};
+  if (!values.brand) {
+    errors.brand = "Required";
+  }
+  if (!values.projectName) {
+    errors.projectName = "Required";
+  }
+  return errors;
+};
 
 function NewProjectModal() {
-    const formik = useFormik({
-        initialValues,
-        onSubmit,
-        validate
-    })
-    console.log('form errors', formik.errors)
-    return (
-        <div>
-             <form onSubmit = {formik.handleSubmit}>
-                  <div className='create-project-modal__brand'>
+  const formik = useFormik({
+    initialValues,
+    onSubmit,
+    validate,
+  });
+
+  return (
+    <div className="newProject-modal__container__container">
+      <div className="newProject-modal">
+        <div className="newProject-modal__body">
+          <div className="newProject-modal__title">New Project</div>
+          <div className="newProject-modal__text">
+            <div className="newProject-modal__main-text">
+              <form onSubmit={formik.handleSubmit}>
+                <div className="create-project-modal__brand">
+                  <label
+                    className="create-project-modal__brand__label"
+                    htmlFor="brand"
+                  >
+                    Brand*
+                  </label>
+                  <input
+                    id="brand"
+                    name="brand"
+                    className="create-project-modal__brand__input"
+                    type="text"
+                    onChange={formik.handleChange}
+                    value={formik.values.brand}
+                  />
+                 {/* {formik.errors.brand ? (
+                    <div>{formik.errors.brand}</div>
+                  ) : null}*/}
+                </div>
+                <div className="create-project-modal__project-name">
+                  <label
+                    className="create-project-modal__project-name__label"
+                    htmlFor="projectName"
+                  >
+                    Project Name*
+                  </label>
+                  <input
+                    id="projectName"
+                    name="projectName"
+                    className="create-project-modal__project-name__input"
+                    type="text"
+                    onChange={formik.handleChange}
+                    value={formik.values.projectName}
+                  />
+                  {/*{formik.errors.projectName ? (
+                   <div>{formik.errors.projectName}</div>
+                  ) : null}}*/}
+                </div>
+                <div className="create-project-modal__project-date">
+                  <div className="create-project-modal__start-date">
                     <label
-                      className='create-project-modal__brand__label'
-                      htmlFor='brand'
+                      className="create-project-modal__start-date__label"
+                      htmlFor="startDate"
                     >
-                      Brand*
+                      Start*
                     </label>
                     <input
-                      id='brand'
-                      name='brand'
-                      className='create-project-modal__brand__input'
-                      type='text'
-                      onChange ={formik.handleChange}
-                      value={formik.values.brand}
+                      id="start-input"
+                      name="startDate"
+                      className="create-project-modal__start-date__input"
+                      placeholder="Date"
+                      type="text"
                     />
                   </div>
-                  {formik.errors.brand ? <div>{formik.errors.brand}</div> : null}
-                  <div className='create-project-modal__project-name'>
+                  <div className="create-project-modal__end-date">
                     <label
-                      className='create-project-modal__project-name__label'
-                      htmlFor='projectName'
+                      className="create-project-modal__end-date__label"
+                      htmlFor="endDate"
                     >
-                      Project Name*
+                      End
                     </label>
                     <input
-                      id='projectName'
-                      name='projectName'
-                      className='create-project-modal__project-name__input'
-                      type='text'
-                      onChange ={formik.handleChange}
-                      value={formik.values.projectName}
+                      id="end-date-input"
+                      name="endDate"
+                      className="create-project-modal__end-date__input"
+                      placeholder="Date"
+                      type="text"
                     />
                   </div>
-                  {formik.errors.projectName? <div>{formik.errors.projectName}</div>:null}
-                  <button type = 'submit'>Sumbit</button>
-                </form>
+                </div>
+                <button className="newProject-modal__button" type="submit">
+                  Save
+                </button>
+                <button className="newProject-modal__button-cancel">
+                  Cancel
+                </button>
+              </form>
+            </div>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export default NewProjectModal
+export default NewProjectModal;
